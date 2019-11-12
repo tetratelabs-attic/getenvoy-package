45b1e142d5e4b4a2874811ed4068d0d61f4b0799
Lizan Zhou <lizan@tetrate.io>
2019-11-12 02:50:44 +0000

    tag rbe-image as latest and use that as default
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- envoy_pkg/python/getenvoy/workspace.py
+++ envoy_pkg/python/getenvoy/workspace.py
@@ -162,9 +162,7 @@ def setupBazelWorkspace(variant):
     with open('WORKSPACE', 'a+') as workspace:
         with open('getenvoy.WORKSPACE') as append:
             workspace.write(append.read().replace(
-                '{RBE_IMAGE_TAG}',
-                os.environ.get('RBE_IMAGE_TAG',
-                               '26527a7d8f6c340c8efcdb0fc70dfea778d2a561')))
+                '{RBE_IMAGE_TAG}', os.environ.get('RBE_IMAGE_TAG', 'latest')))
 
 
 def setup(args):
