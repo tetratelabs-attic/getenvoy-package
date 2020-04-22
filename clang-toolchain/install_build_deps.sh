6082756fa7c46fff30aac59fd0e2f67e4d6cd508
Lizan Zhou <lizan@tetrate.io>
2020-04-22 00:00:26 +0000

    fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- clang-toolchain/install_build_deps.sh
+++ clang-toolchain/install_build_deps.sh
@@ -19,7 +19,7 @@ set -e
 yum install -y centos-release-scl epel-release
 yum update -y
 yum install -y devtoolset-9-gcc devtoolset-9-gcc-c++ devtoolset-9-libatomic-devel wget unzip which make cmake3 patch subversion ncurses-devel zlib-devel \
-  python-virtualenv chrpath file perl-Data-Dumper tcl python2-psutil sudo
+  python-virtualenv chrpath file perl-Data-Dumper tcl python2-psutil sudo rsync
 
 # For LLVM to pick right libstdc++
 mkdir -p /usr/lib/gcc/x86_64-redhat-linux
