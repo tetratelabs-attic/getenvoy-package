b745270be1274377390dd936b9ad260728c8b9e6
Lizan Zhou <lizan@tetrate.io>
2020-08-27 17:56:13 -0700

    install gn for wasm build
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- linux-glibc/build_container/getenvoy_linux_glibc_rbe.sh
+++ linux-glibc/build_container/getenvoy_linux_glibc_rbe.sh
@@ -35,6 +35,7 @@ ln -s /opt/rh/devtoolset-7/root/usr/lib/gcc/x86_64-redhat-linux/7 /usr/lib/gcc/x
 
 # httpd24 is equired by rh-git218
 echo "/opt/rh/httpd24/root/usr/lib64" > /etc/ld.so.conf.d/httpd24.conf
+ldconfig
 
 # For RPM package
 yum install -y rpm-build rpm-sign
@@ -42,8 +43,26 @@ yum install -y rpm-build rpm-sign
 yum clean all
 
 # bazelisk
-VERSION=1.5.0
-SHA256=605c4b417654a09febc7e15c6b802195fdd1c4fe8d876145fb15f10d4fa27b5b
+VERSION=1.6.0
+SHA256=616f65bcdcfd134a19d5d86c591c35098d7732be26145bf06f02ec9e3e52700c
 curl --location --output /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-amd64 \
   && echo "$SHA256  /usr/local/bin/bazel" | sha256sum --check \
   && chmod +x /usr/local/bin/bazel
+
+# gn
+mkdir -p gn
+pushd gn
+
+bash -exc "
+source /opt/rh/rh-git218/enable
+source /opt/rh/devtoolset-7/enable
+
+git clone https://gn.googlesource.com/gn .
+git checkout 5ed3c9cc67b090d5e311e4bd2aba072173e82db9
+python build/gen.py
+ninja -C out
+cp out/gn /usr/local/bin
+"
+
+popd
+rm -rf gn
