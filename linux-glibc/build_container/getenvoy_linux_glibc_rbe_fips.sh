e812f5313f38492a23e860b8fb808c0e815a960d
Lizan Zhou <lizan@tetrate.io>
2021-01-15 01:57:59 -0800

    fix
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

---
+++ linux-glibc/build_container/getenvoy_linux_glibc_rbe_fips.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2019 Tetrate
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -sSL https://releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz | \
+  tar Jx --strip-components=1 -C /usr/local
+
+# Force libc++ to be a static link by putting a linker script to do that.
+echo 'INPUT(-l:libc++.a -l:libc++abi.a -lm -lpthread)' > /usr/local/lib/libc++.so
